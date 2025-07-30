0c974c1a0193cb3e7d5f47525a8b98a9312da97b
Iaroslav Sannikov <sannikov_yao@farpost.com>
2025-07-30 23:14:11 +1000

    Refactor
    Change arrays of obects into just arrays

--- test/app.test.js
+++ test/app.test.js
@@ -1,43 +1,28 @@
 import { nameIsValid, fullTrim, getTotal } from "../src/app.js";
 
 describe('nameIsValid function', () => {
- const validCases = [
-  {name: 'dudoser', expected: true},
-  {name: 'fr', expected: true},
-];
 
-const invalidCases = [
-  {name: 'x'},
-  {name: 'Fr'},
-  {name: 'eric cartman'},
-  {name: null},
-  {name: undefined},
-  {name: ''},
-  {name: 707},
-  {name: null},
-];
+  const validCases = ['dudoser', 'fr'];
 
-  test.each(validCases)('$name should be valid', ({ name }) => {
+  const invalidCases = ['x', 'Fr', 'eric cartman', null, undefined, ' ', '', 707];
+
+  test.each(validCases)('"%s" should be valid', (name) => {
     expect(nameIsValid(name)).toBe(true);
   });
 
-  test.each(invalidCases)('$name should be invalid', ({ name }) => {
+  test.each(invalidCases)('"%s" should be invalid', (name) => {
     expect(nameIsValid(name)).toBe(false);
   });
 });
 
 describe('fullTrim function', () => {
-  const returnEmptyString = [
-    {input: null},
-    {input: undefined},
-    {input: ''}
-  ]
+  const returnEmptyString = [null, undefined, ''];
 
   test('should remove all spaces from a string', () => {
     expect(fullTrim('h e l l o !')).toBe('hello!');
   });
 
-  test.each(returnEmptyString)('should return empty string for $input', ({ input }) => {
+  test.each(returnEmptyString)('should return empty string for "%s"', (input) => {
     expect(fullTrim(input)).toBe('');
   });
 
