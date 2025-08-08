ba686c0a58c610b8810001bfdf4cc6ee826486da
Iaroslav Sannikov <sannikov_yao@farpost.com>
2025-08-09 09:35:29 +1000

    Add config files for bookstore api

---
+++ framework/config/config.js
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+
+export default Object.freeze({
+  baseURL: process.env.BOOKSTORE_API_URL,
+  userId: process.env.BOOKSTORE_USER_ID,
+  username: process.env.BOOKSTORE_USERNAME,
+  password: process.env.BOOKSTORE_PASSWORD
+})
---
+++ framework/config/index.js
@@ -0,0 +1,1 @@
+export {default as config} from '../framework/config/config.js'
