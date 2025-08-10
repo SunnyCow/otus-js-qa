095723a5844f39153fd2904de3d1aee56d5e2b83
Iaroslav Sannikov <sannikov_yao@farpost.com>
2025-08-10 10:07:56 +1000

    Add new tests for bookstore api and refactor old ones

--- test/api.spec.js
+++ test/api.spec.js
@@ -1,14 +1,38 @@
-import config from "../framework/config/config";
-import UserService from "../framework/services/UserService";
-import AuthService from "../framework/services/AuthService";
-import { generateUserCredentials } from "../framework/fixtures/userFixture";
+import UserService from '../framework/services/UserService';
+import AuthService from '../framework/services/AuthService';
+import generateUserCredentials from '../framework/fixtures/userFixture';
+import createAndAuthUser from '../framework/utils/createAndAuthUser';
 
 describe('Bookstore API - User creation tests', () => {
+  test('should create user with valid credentials', async () => {
+    const user = generateUserCredentials();
+    const response = await UserService.create(user);
+
+    expect(response.status).toBe(201);
+    expect(response.data).toHaveProperty('userID');
+    expect(response.data.username).toBe(user.username);
+  });
+
+  test('should get info about user', async () => {
+    const { user, userResponse, tokenResponse } = await createAndAuthUser();
+    const response = await UserService.get(userResponse.data.userID, tokenResponse.data.token);
+
+    expect(response.status).toBe(200);
+    expect(response.data.books).toBeDefined();
+    expect(response.data.username).toBe(user.username);
+    expect(response.data.userID).toBe(tokenResponse.data.userID);
+  });
+
+  test('should falit to get info about user without token', async () => {
+    const { userResponse } = await createAndAuthUser();
+    const response = await UserService.get(userResponse.data.userID);
+
+    expect(response.status).toBe(401);
+    expect(response.data.message).toBe('User not authorized!');
+  });
+
   test('should fail to create user if username is already taken', async () => {
-    const credentials = {
-      username: config.username,
-      password: config.password
-    };
+    const credentials = generateUserCredentials();
 
     await UserService.create(credentials);
 
@@ -19,51 +43,80 @@ describe('Bookstore API - User creation tests', () => {
   });
 
   test('should fail to create user with weak password', async () => {
-    const response = await UserService.create({
-        username: 'weakPasswordUser',
-        password: '0123'
-      });
+    const response = await UserService.create(generateUserCredentials({ valid: false, reason: 'weakPassword' }));
 
     expect(response.status).toBe(400);
     expect(response.data.message).toMatch(/Passwords must have/);
   });
 
-  test('should create user', async () => {
-    const user = generateUserCredentials();
+  test('should delete user with valid uuid and token', async () => {
+    const { userResponse, tokenResponse } = await createAndAuthUser();
+    const response = await UserService.delete(userResponse.data.userID, tokenResponse.data.token);
 
-    const response = await UserService.create(user);
-
-    expect(response.status).toBe(201);
-    expect(response.data).toHaveProperty('userID');
-    expect(response.data.username).toBe(user.username);
+    expect(response.status).toBe(204);
+    expect(response.data).toBe('');
   });
 
-});
-
-describe('Bookstore API - token tests', () => {
-  test('should fail to generate token with invalid credentials', async () => {
-    const username = `user${Date.now()}`;
-    const password = '123';
-
-    const response = await AuthService.generateToken({
-      username,
-      password
-    });
+  test("should fail to delete user if uuid and token don't match", async () => {
+    const { userResponse } = await createAndAuthUser();
+    const { tokenResponse: otherUserTokenResponse } = await createAndAuthUser();
+    const response = await UserService.delete(userResponse.data.userID, otherUserTokenResponse.data.token);
 
     expect(response.status).toBe(200);
-    expect(response.data.status).toBe('Failed');
-    expect(response.data.result).toMatch(/User authorization failed/);
+    expect(response.data.message).toMatch(/user id not correct!/i);
   });
+});
 
+describe('Bookstore API - authorization tests', () => {
   test('should generate token with valid credentials', async () => {
-    const credentials = generateUserCredentials();
-
-    await UserService.create(credentials);
-
-    const response = await AuthService.generateToken(credentials);
+    const { tokenResponse: response } = await createAndAuthUser();
 
     expect(response.status).toBe(200);
     expect(response.data.status).toBe('Success');
     expect(response.data.token).toBeDefined();
+  });
+
+  test('should authorize user with valid credentials', async () => {
+    const { user } = await createAndAuthUser();
+    const response = await AuthService.isAuthorized(user);
+
+    expect(response.status).toBe(200);
+    expect(response.data).toBe(true);
+  });
+
+  test('should fail to authorize nonexistent user', async () => {
+    const credentials = generateUserCredentials();
+    const response = await AuthService.generateToken(credentials);
+
+    expect(response.status).toBe(200);
+    expect(response.data.expires).toBeNull();
+    expect(response.data.status).toBe('Failed');
+    expect(response.data.token).toBeNull();
+  });
+
+  test('should fail to authorize user without token', async () => {
+    const credentials = generateUserCredentials();
+    await UserService.create(credentials);
+
+    const response = await AuthService.isAuthorized(credentials);
+
+    expect(response.status).toBe(200);
+    expect(response.data).toBe(false);
+  });
+
+  test('should fail to generate token without password', async () => {
+    const credentials = generateUserCredentials({ valid: false, reason: 'missingPassword' });
+    const response = await AuthService.generateToken(credentials);
+
+    expect(response.status).toBe(400);
+    expect(response.data.message).toMatch(/username and password required/i);
+  });
+
+  test('should fail to generate token without username', async () => {
+    const credentials = generateUserCredentials({ valid: false, reason: 'missingUsername' });
+    const response = await AuthService.generateToken(credentials);
+
+    expect(response.status).toBe(400);
+    expect(response.data.message).toMatch(/username and password required/i);
   });
 });
