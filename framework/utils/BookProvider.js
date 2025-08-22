46134d2884d8b7e49e98e87eef23c46bb58aeb9f
Iaroslav Sannikov <sannikov_yao@farpost.com>
2025-08-23 00:37:56 +1000

    Add function to get isbns to BookProvider

--- framework/utils/BookProvider.js
+++ framework/utils/BookProvider.js
@@ -4,10 +4,12 @@ const getAll = () => booksJson.books;
 
 const getRange = (start, end) => booksJson.books.slice(start, end);
 
+const getIsbns = (num) => booksJson.books.slice(0, num).map((book) => book.isbn);
+
 const getRandomIsbn = () => {
   const isbns = booksJson.books.map((book) => book.isbn);
   const index = Math.floor(Math.random() * isbns.length);
   return isbns[index];
 };
 
-export default { getAll, getRange, getRandomIsbn };
+export default { getAll, getRange, getIsbns, getRandomIsbn };
