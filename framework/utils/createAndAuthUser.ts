ec74d251f21616deb626c2b1eaf7d12d2b61454c
Iaroslav Sannikov <sannikov_yao@farpost.com>
2025-09-07 13:58:08 +1000

    Refactor untility functions

--- framework/utils/createAndAuthUser.ts
+++ framework/utils/createAndAuthUser.ts
@@ -1,8 +1,13 @@
 import generateUserCredentials from '../fixtures/userFixture';
 import UserService from '../services/UserService';
 import AuthService from '../services/AuthService';
+import type { User, AuthCredentials, AuthToken, ApiResponse } from '../../types';
 
-async function createAndAuthUser() {
+async function createAndAuthUser(): Promise<{
+  user: AuthCredentials;
+  userResponse: ApiResponse<User>;
+  tokenResponse: ApiResponse<AuthToken>;
+}> {
   const user = generateUserCredentials();
   const userResponse = await UserService.create(user);
   const tokenResponse = await AuthService.generateToken(user);
