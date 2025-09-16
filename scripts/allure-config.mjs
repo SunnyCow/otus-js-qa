19446dc352672e77531ea287d96c39450552bb84
Iaroslav Sannikov <sannikov_yao@farpost.com>
2025-09-16 16:49:42 +1000

    Another try

---
+++ scripts/allure-config.mjs
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+
+const config = Object.freeze({
+  // https://${TOKEN}@github.com/your-user/your-repo.git
+  repoUrl: process.env.TEST_ALLURE_REPO_URL,
+  branchName: process.env.TEST_ALLURE_BRANCH_NAME ?? 'allure-history',
+  historyDir: process.env.TEST_ALLURE_HISTORY_DIR ?? 'history'
+});
+
+export default config;
--- scripts/allure.ci.zx.mjs
+++ scripts/allure.ci.zx.mjs
@@ -7,6 +7,9 @@ const ALLURE_RESULT_PATH = process.env.ALLURE_RESULT_PATH ?? './reports/allure-r
 const ALLURE_REPORT_PATH = process.env.ALLURE_REPORT_PATH ?? './reports/allure-report';
 
 await fs.remove(ALLURE_RESULT_PATH);
-await $`npm test`.nothrow();
-await $`node scripts/allure-history.mjs`;
+const exitCode = await $`npm test`.nothrow().exitCode;
+await $`node scripts/allure-load.mjs`;
 await $`npx allure generate ${ALLURE_RESULT_PATH} --clean --report-dir ${ALLURE_REPORT_PATH}`;
+await $`node scripts/allure-upload.mjs`;
+
+process.exit(exitCode);
